8420a30f7ba635cb9fa6bc821d8723010e996a15
amazing-hash <amazing-hash@yandex.ru>
2024-02-01 20:09:13 +0400

    build: ffmpeg dependecies

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+extern "C"{
+    #include <libavdevice/avdevice.h>
+}
+
 
 int main() {
+    avdevice_register_all();
+    avformat_network_init();
+    av_log_set_level(AV_LOG_ERROR);
+
     std::cout << "Hello Axomavis" << std::endl;
     return 0;
 }
